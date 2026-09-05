58d19381ea21cc59112b427fb7158885a5491206
MahdiMuttaqi <mehdi.muttaqi2014@gmail.com>
2026-09-05 15:08:06 +0330

    Restore direct Apps Script connection

--- web/config.js
+++ web/config.js
@@ -1,1 +1,1 @@
-window.NEBRAS_API_URL = "https://nebras-excel-bridge.mahmood-shohani.chatgpt.site/api";
+window.NEBRAS_API_URL = "https://script.google.com/macros/s/AKfycbxf5KpsKaDgWTqCy7dn2w_JTtqYj06r585TTx4iFa1sgY_aLicEqIixyJHJ2DiLiA/exec";
