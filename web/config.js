267693f1eda808afc2d198e161d2f660a0474d5d
MahdiMuttaqi <mehdi.muttaqi2014@gmail.com>
2026-09-12 09:13:15 +0330

    Update production tracking: web/config.js

--- web/config.js
+++ web/config.js
@@ -1,1 +1,1 @@
-window.NEBRAS_API_URL = "https://script.google.com/macros/s/AKfycbxf5KpsKaDgWTqCy7dn2w_JTtqYj06r585TTx4iFa1sgY_aLicEqIixyJHJ2DiLiA/exec";
+window.NEBRAS_API_URL = "https://script.google.com/macros/s/AKfycbzYBQLkkGCr8sdMzg4ec_xTlDnOkvWqJqyrZCYvYVqZnr9sdWSWrOKXCS8c6rHrD668/exec";
